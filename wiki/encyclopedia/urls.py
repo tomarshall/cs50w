920b3b0a43099e2401ef5d67d531e3c8d80ac7b5
Tommie Marshall <55310447+tomarshall@users.noreply.github.com>
2024-07-24 19:51:04 +0100

    Implemented markdown to HTML conversion and page search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("search", views.search, name="search"),
+    path("<str:title>", views.entry, name="entry"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import markdown2
 
 from django.shortcuts import render
 
@@ -11,12 +12,32 @@ def index(request):
     })
 
 def entry(request, title):
-    #if (content := util.get_entry(title)):
-
-    #else:
-        # Error message "Page not found"
-        
+    # If entry not found, show error
+    if not (entry := util.get_entry(title)):
+        title = "Error"
+        entry = "Error: Page not found."
+    # Otherwise show entry's markdown contents
     return render(request, "encyclopedia/entry.html", {
         "title": title,
-        "content": util.get_entry(title),
+        "content": markdown2.markdown(entry),
     })
+
+def search(request):
+    query = request.GET.get("q")
+    # If there is an entry that matches that title
+    if query in util.list_entries():
+        entry = util.get_entry(query)
+        return render(request, "encyclopedia/entry.html", {
+            "title": query,
+            "content": markdown2.markdown(entry)
+        })
+    else:
+        # List for pages with matching substring
+        matches = []
+        # Check each entry for presence of substring
+        for title in util.list_entries():
+            if query in markdown2.markdown(util.get_entry(title)):
+                matches.append(title)
+        return render(request, "encyclopedia/search.html", {
+            "matches": matches
+        })
