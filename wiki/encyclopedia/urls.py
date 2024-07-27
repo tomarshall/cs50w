316a8cbe41e37789cdcee616940cbf15b3738c59
Tommie Marshall <55310447+tomarshall@users.noreply.github.com>
2024-07-27 16:10:44 +0100

    Added newpage to urlpatterns

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage"),
     path("<str:title>", views.entry, name="entry"),
 ]
