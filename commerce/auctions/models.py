a10805f281e77f0e54eb254b93f533bddd139f5a
Tommie Marshall <55310447+tomarshall@users.noreply.github.com>
2024-07-29 19:24:32 +0100

    Set up categories model in Django Admin

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,31 @@
+from django.contrib import admin
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    pass
+
+
+class Bid(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+# Category i.e. Fashion, Toys, Electronics, Home, etc.
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
